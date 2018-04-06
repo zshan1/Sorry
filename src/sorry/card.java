bdbcfac2fe09b58ec923bfb6ee4f100a3a0fa813
William Dean Wertz <william.wertz@uvm.edu>
2018-04-06 15:53:23 -0400

    test

--- src/sorry/card.java
+++ src/sorry/card.java
@@ -3,6 +3,7 @@ package sorry;
 public class card {
 
 	public static void main(String[] args) {
-		System.out.println("Where are these cards?!?")
+		System.out.println("Where are these cards?!?");
+		System.out.println("They're right here");
 	}
 }
