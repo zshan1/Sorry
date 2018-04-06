46f341a93fdba752a25fa35976194eb1a0fe4160
William Dean Wertz <william.wertz@uvm.edu>
2018-04-06 15:58:30 -0400

    test

--- src/sorry/card.java
+++ src/sorry/card.java
@@ -5,5 +5,7 @@ public class card {
 	public static void main(String[] args) {
 		System.out.println("Where are these cards?!?");
 		System.out.println("They're right here");
+		System.out.println("They're right here");
+		System.out.println("They're right here");
 	}
 }
